646cdefc26b0878fe3ec7407cb544695efc4be77
thatdania@gmail.com <thatdania@gmail.com>
2018-01-09 10:30:43 +0000

    Create server.js file
    Although we started halfway in making the server.js, we hesistated as the guides we were following were not the directions we wanted to go in.
    
    Driver has changed to someone else (Pair)

---
+++ src/server.js
@@ -0,0 +1,13 @@
+// `use strict`
+//
+// var express = require(`express`);
+// var mongoose = require(`mongoose`);
+// var bodyParser = require (`body-parser`);
+//
+// var app = express();
+// var router = express.Router();
+//
+// var port = process.env.API_PORT || 3001;
+//
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
