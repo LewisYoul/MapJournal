117b6df4a4be28abf48a12b194ae2585b86b7aeb
Lewis <lewisyoul@gmail.com>
2018-01-09 10:00:45 +0000

    set up test framework
    installed enzyme, chai, sinon

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow, mount, render } from 'enzyme';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+chai.use(chaiEnzyme());
+
+import App, { Header } from './App';
+
+it('1 add 2 equals 3', () => {
+  expect(1 + 2).to.equal(3)
 });
